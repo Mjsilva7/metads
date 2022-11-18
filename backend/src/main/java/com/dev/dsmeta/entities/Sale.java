fecb3ea1b8f04d9ea4a6f02cf21477d3e42c1a47
Mjsilva7 <mj.silva7@gmail.com>
2022-11-18 15:22:00 -0300

    Database

---
+++ backend/src/main/java/com/dev/dsmeta/entities/Sale.java
@@ -0,0 +1,32 @@
+package com.dev.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_sales")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+}
