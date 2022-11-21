109126ee9c0de1971501170ce8a9cdb7b0e12b30
Mjsilva7 <mj.silva7@gmail.com>
2022-11-21 16:46:03 -0300

    Twilio SMS

--- backend/src/main/java/com/dev/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/dev/dsmeta/entities/Sale.java
@@ -29,4 +29,43 @@ public class Sale {
     private Integer deals;
     private Double amount;
     private LocalDate date;
+    
+	public String getSellerName() {
+		return sellerName;
+	}
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getVisited() {
+		return visited;
+	}
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+	public Integer getDeals() {
+		return deals;
+	}
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	
 }
